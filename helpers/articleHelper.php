c4dfb2cde65019bda762833530f102171c28e5be
Jonathan Kim <jkimbo@gmail.com>
2012-03-25 22:08:06 +0100

    Added category to article api

--- helpers/articleHelper.php
+++ helpers/articleHelper.php
@@ -3,18 +3,33 @@
 class ArticleHelper extends BaseHelper {
     public function getOutput() {
         $output = parent::getOutput();
+
+        // authors
         unset($output['author']);
         $output['authors'] = array();
         foreach($this->this->getAuthors() as $key => $author) {
             $output['authors'][$key] = $author->getOutput();
         }
 
+        // approved by
         $output['approvedby'] = $this->this->getApprovedBy()->getOutput();
 
+        // content
+        unset($output['text1']);
+        unset($output['text2']);
+        $output['content'] = $this->this->getContent();
+
+        // image
+        unset($output['img1']);
+        unset($output['img2']);
+        unset($output['img2lr']);
         $output['image'] = '';
         if($this->this->getImage()) {
             $output['image'] = $this->this->getImage()->getOutput();
         }
+
+        // category
+        $output['category'] = $this->this->getCategory()->getOutput();
         return $output;
     }
 }
