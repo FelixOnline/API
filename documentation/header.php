3d91460d1141e6cff4c059fd887bf75ac9c7c25c
Jonathan Kim <jkimbo@gmail.com>
2012-04-05 14:29:18 +0100

    Updated header and footer

--- documentation/header.php
+++ documentation/header.php
@@ -1,33 +1,19 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
 
-    <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/b/378 -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
     <title>Felix Online API Documentation</title>
     <meta name="description" content="The Felix Online API documentation">
     <meta name="author" content="Jonathan Kim">
 
-    <!-- Facebook Meta -->
-    <meta property="og:title" content=""/>
-    <meta property="og:url" content=""/>
-    <meta property="og:description" content=""/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:image" content="img/logo.jpg"/>
-    <meta property="fb:admins" content="" />
-
-    <!-- Mobile viewport optimized: j.mp/bplateviewport -->
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-
-    <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+    <!-- Mobile viewport optimized: h5bp.com/viewport -->
+    <meta name="viewport" content="width=device-width">
 
     <!-- LESS: implied media="all" -->
     <link rel="stylesheet/less" type="text/css/" href="<?php echo API_DOCS_URL; ?>css/bootstrap.less">
@@ -37,9 +23,9 @@
 
     <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
 
-    <!-- All JavaScript at the bottom, except for this custom Modernizr build containing Respond.
-       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
-       For optimal performance, create your own custom Modernizr build: www.modernizr.com/download/ -->
-    <script src="<?php echo API_DOCS_URL; ?>js/libs/modernizr-2.5.3.min.js"></script>
+    <!-- All JavaScript at the bottom, except this Modernizr build.
+       Modernizr enables HTML5 elements & feature detects for optimal performance.
+       Create your own custom Modernizr build: www.modernizr.com/download/ -->
+    <script src="js/libs/modernizr-2.5.3.min.js"></script>
 </head>
 <body>
