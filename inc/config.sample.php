294e066ba379102b28b1a7017130f5d813bcedb8
Jonathan Kim <jkimbo@gmail.com>
2013-12-29 15:09:09 +0000

    Updated config sample

--- inc/config.sample.php
+++ inc/config.sample.php
@@ -3,12 +3,15 @@
      * Create a config.inc.php with the information below to run on a local dev machine
      */
 
-    $db = "DB_TABLE";
-    $host = "localhost";
-    $user = "DB_USER";
-    $pass = "DB_PASSWORD";
-    $cid = mysql_connect($host,$user,$pass);
-    $dbok = mysql_select_db($db,$cid);
+    $config = array(
+        'db_name' => 'DB_TABLE',
+        'db_user' => 'DB_USER',
+        'db_pass' => 'DB_PASS',
+        'base_url' => 'http://felixonline.co.uk/',
+    );
+
+    $cid = mysql_connect('localhost', $config['db_user'], $config['db_pass']);
+    $dbok = mysql_select_db($config['db_name'], $cid);
 
     /* Forces charset to be utf8 */
     mysql_set_charset('utf8',$cid);
@@ -30,5 +33,6 @@
 
     define('PRODUCTION_FLAG', true); // if set to true css and js will be minified etc..
     define('AUTHENTICATION', true); // if set to false then authentication will be turned off across the site. ONLY TURN OFF ON LOCAL SERVER
-	define('AUTHENTICATION_PATH','https://dougal.union.ic.ac.uk/media/felix/'); // change authentication path to local file for local dev (default is https://dougal.union.ic.ac.uk/media/felix/)
-?>
+    define('AUTHENTICATION_PATH','https://dougal.union.ic.ac.uk/media/felix/'); // change authentication path to local file for local dev (default is https://dougal.union.ic.ac.uk/media/felix/)
+
+    return $config;
