11649a3bf80e05159300ae08785119197bb0ba98
Philip Kent <philip.kent@me.com>
2015-02-24 11:26:20 +0000

    Support HEAD method for pingdom

--- controllers/baseController.php
+++ controllers/baseController.php
@@ -8,5 +8,11 @@ class BaseController {
     function __construct() {
         API::log_api_request(get_class($this));
     }
+
+	function HEAD($matches)
+	{
+		// Used by updowntester
+		exit;
+	}
 }
 ?>
